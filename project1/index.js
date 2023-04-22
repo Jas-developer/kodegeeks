0824cb6d7dd4324ec04cd2d79c83fdd84821db82
Jason <evaristojason25@gmail.com>
2023-04-22 19:22:36 +0800

    I change my project1

---
+++ project1/index.js
@@ -0,0 +1,7 @@
+const searchButton = document.getElementById("#search-button");
+const searchInput = document.querySelector("#search-input");
+
+searchButton.addEventListener("click", function () {
+  searchInput.style.borderColor = "orange";
+});
+document.getElementById;
