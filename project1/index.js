0faa90999669c2e85a7d8370383bb27bcf7016be
Jason <evaristojason25@gmail.com>
2023-04-24 15:48:28 +0800

    BUTTON HOVER IMPLEMENTED

--- project1/index.js
+++
@@ -1,7 +0,0 @@
-const searchButton = document.getElementById("#search-button");
-const searchInput = document.querySelector("#search-input");
-
-searchButton.addEventListener("click", function () {
-  searchInput.style.borderColor = "orange";
-});
-document.getElementById;
